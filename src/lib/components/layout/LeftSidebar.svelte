33c9698d1cfafb1e7ebd3239f17f197642f78a1f
Hasibuzzaman Tonmoy <hsb.tonmoy@gmail.com>
2023-06-01 15:14:40 -0400

    Init. Layout

---
+++ src/lib/components/layout/LeftSidebar.svelte
@@ -0,0 +1,14 @@
+<section class="flex flex-col">
+	<div class="flex items-center justify-between">
+		<h1 class="h1">
+			<span
+				class="bg-gradient-to-br from-gray-400 to-white bg-clip-text text-transparent box-decoration-clone uppercase font-semibold text-3xl"
+				>Tonmoy.</span
+			>
+		</h1>
+		<div class="flex flex-col gap-1">
+			<span class="text-gray-300 text-xs">Software Developer</span>
+			<span class="text-gray-300 text-xs text-right">& Data Analyst</span>
+		</div>
+	</div>
+</section>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import LeftSidebar from '$lib/components/layout/LeftSidebar.svelte';
+</script>
+
+<AppShell
+	slotSidebarLeft="w-auto flex items-center ml-6 my-20"
+	slotSidebarRight="w-auto flex items-center mr-20"
+>
+	<!-- (header) -->
+	<svelte:fragment slot="sidebarLeft"
+		><div class="border-[0.5px] border-gray-500 rounded-xl h-full px-10 py-12">
+			<LeftSidebar />
+		</div></svelte:fragment
+	>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<slot />
+</AppShell>
