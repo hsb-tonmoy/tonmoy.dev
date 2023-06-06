7cb4a23d5757c9c17e1e9f322549981c5583f4e7
Hasibuzzaman Tonmoy <hsb.tonmoy@gmail.com>
2023-06-06 14:07:01 -0400

    added right menu container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,21 +7,61 @@
 	import '../app.postcss';
 
 	import LeftSidebar from '$lib/components/layout/LeftSidebar.svelte';
+	import Menu from '$lib/icons/Menu.svelte';
 </script>
 
-<div class="w-full h-full flex">
+<div class="relative w-full min-h-screen flex flex-col xl:flex-row bg-surface-600 p-4 xl:p-0">
 	<aside
 		id="leftSidebar"
-		class="xl:w-auto w-full flex items-start xl:items-center px-6 py-4 xl:py-20"
+		class="xl:sticky h-full top-20 bottom-20 left-6 flex-none xl:w-auto w-full flex items-start xl:items-center"
 	>
 		<div class="w-full border-[0.5px] border-surface-500 rounded-3xl xl:h-full px-6 md:px-10 py-12">
 			<LeftSidebar />
 		</div>
 	</aside>
-	<main class="w-full h-full flex overflow-hidden py-20">
+	<main class="flex-1 min-w-0 overflow-auto py-20">
 		<slot />
 	</main>
-	<aside id="rightMenu" class="flex-none overflow-x-hidden overflow-y-auto">
-		<div class="hidden xl:flex w-auto items-center mr-20 py-20">Sidebar Right</div>
+	<aside
+		id="rightMenu"
+		class="fixed xl:sticky xl:h-screen top-2 right-2 xl:right-16 pt-20 flex-none xl:w-auto w-full flex items-end xl:items-center flex-col"
+	>
+		<button
+			class="border-[0.5px] bg-surface-600 border-surface-500 hover:border-white rounded-full w-14 h-14 p-2 transition-all ease-in-out duration-300"
+		>
+			<span class="text-white">
+				<Menu />
+			</span>
+		</button>
+		<div class="hidden xl:flex">
+			<div
+				class="flex flex-col gap-4 justify-center border-[0.5px] border-surface-500 rounded-full h-full p-4 text-white"
+			>
+				<span class="w-5 h-5">
+					<Menu />
+				</span>
+				<span class="w-5 h-5">
+					<Menu />
+				</span>
+				<span class="w-5 h-5">
+					<Menu />
+				</span>
+				<span class="w-5 h-5">
+					<Menu />
+				</span>
+				<span class="w-5 h-5">
+					<Menu />
+				</span>
+				<span class="w-5 h-5">
+					<Menu />
+				</span>
+				<span class="w-5 h-5">
+					<Menu />
+				</span>
+				<span class="w-5 h-5">
+					<Menu />
+				</span>
+			</div>
+		</div>
 	</aside>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Home from '$lib/icons/Home.svelte';
 </script>
 
-<div class="container w-full xl:w-auto xl:max-w-3xl h-full mx-auto flex">
+<div class="container w-full xl:w-auto xl:max-w-3xl h-full mx-auto flex flex-col">
 	<div class="flex flex-col">
 		<button class="btn btn-md variant-ringed-surface text-white uppercase text-xs w-fit mb-14"
 			><span class="w-4 h-4"><Home /></span> <span>Introduction</span></button
@@ -10,7 +10,7 @@
 		<h2 class="text-white text-7xl leading-[1.15] font-light">
 			Hi from <span class="text-primary-500">Tonmoy</span>,<br /> Software and<br /> Full-stack Developer
 		</h2>
-		<div class="max-w-lg mt-12">
+		<div class="max-w-xl mt-12">
 			<span class="text-textColor"
 				>In the world of web development, I find my joy. I'm Hasibuzzaman Tonmoy, designing
 				simplicity and coding innovation.</span
