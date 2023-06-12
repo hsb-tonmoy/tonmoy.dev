9548d0c3698cdd2f1031875b309afbf3c63f5020
Hasibuzzaman Tonmoy <hsb.tonmoy@gmail.com>
2023-06-12 10:27:58 -0400

    removed animation

--- src/routes/(root)/About.svelte
+++ src/routes/(root)/About.svelte
@@ -1,27 +1,9 @@
-<script lang='ts'>
-	import { inview } from 'svelte-inview';
-  import type { ObserverEventDetails, ScrollDirection, Options } from 'svelte-inview';
-
-  let isInView: boolean;
-  let scrollDirection: ScrollDirection;
-  const options: Options = {
-    rootMargin: '-20px',
-    unobserveOnEnter: true,
-  };
-
-  const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
-    isInView = detail.inView;
-    scrollDirection = detail.scrollDirection.vertical;
-  };
-
+<script lang="ts">
 	import TitleChip from '$lib/components/layout/TitleChip.svelte';
-	import MaterialSymbolsPerson from '~icons/material-symbols/person'
-
+	import MaterialSymbolsPerson from '~icons/material-symbols/person';
 </script>
 
-<div use:inview={options} on:inview_change={handleChange}  class:animate__animated={isInView} 
-class:animate__fadeInDown={scrollDirection === 'down'}
-class:animate__fadeInUp={scrollDirection === 'up'} class="flex flex-col py-20">
+<div class="flex flex-col py-20">
 	<TitleChip icon={MaterialSymbolsPerson} title="About" />
 	<h2 class="text-white text-2xl md:text-5xl font-light mb-8">
 		Crafting the Future, One Line of <span class="text-primary-500">Code</span> at a Time
--- src/routes/(root)/Resume.svelte
+++ src/routes/(root)/Resume.svelte
@@ -1,33 +1,15 @@
-<script lang='ts'>
-	import { inview } from 'svelte-inview';
-  import type { ObserverEventDetails, ScrollDirection, Options } from 'svelte-inview';
-
-  let isInView: boolean;
-  let scrollDirection: ScrollDirection;
-  const options: Options = {
-    rootMargin: '-20px',
-    unobserveOnEnter: true,
-  };
-
-  const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
-    isInView = detail.inView;
-    scrollDirection = detail.scrollDirection.vertical;
-  };
-
+<script lang="ts">
 	import TitleChip from '$lib/components/layout/TitleChip.svelte';
-	import MaterialSymbolsLabProfile from '~icons/material-symbols/lab-profile'
-	import MaterialSymbolsDownload from '~icons/material-symbols/download'
+	import MaterialSymbolsLabProfile from '~icons/material-symbols/lab-profile';
+	import MaterialSymbolsDownload from '~icons/material-symbols/download';
 </script>
 
-
-<div use:inview={options} on:inview_change={handleChange} class:animate__animated={isInView} 
-class:animate__fadeInDown={scrollDirection === 'down'}
-class:animate__fadeInUp={scrollDirection === 'up'} class="flex flex-col py-20">
+<div class="flex flex-col py-20">
 	<TitleChip icon={MaterialSymbolsLabProfile} title="Resume" />
-	<h2 class="text-white text-2xl md:text-5xl font-light mb-16">
+	<h2 class="text-white text-2xl md:text-5xl font-light mb-16 duration-500">
 		Work <span class="text-primary-500">Experiences</span>
 	</h2>
-	<ol class="relative border-l border-surface-500 ml-4">
+	<ol class="relative border-l border-surface-500 ml-4 duration-500">
 		<li class="group mb-10 ml-10 md:ml-16">
 			<span
 				class="absolute flex items-center justify-center w-4 h-4 md:w-6 md:h-6 rounded-full -left-[0.55rem] md:-left-[0.80rem] bg-surface-500 group-hover:ring-8 group-hover:ring-primary-600 group-hover:bg-primary-900 transition-all ease-in-out duration-300"
