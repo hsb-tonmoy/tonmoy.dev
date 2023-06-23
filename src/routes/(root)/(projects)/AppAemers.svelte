ddc492dff5c8bcf508ed20cac9e9b4477fe3ec00
Hasibuzzaman Tonmoy <hsb.tonmoy@gmail.com>
2023-06-23 15:59:28 -0400

    added app-aemers project

---
+++ src/routes/(root)/(projects)/AppAemers.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { filter, GreenFall } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="">
+	<div
+		class="relative flex justify-center items-center bg-[#0e0f14] rounded-t-lg w-full overflow-hidden"
+	>
+		<img
+			src="/images/projects/app-aemers/app-aemers-login.png"
+			alt="Project"
+			class="absolute object-fill w-full -rotate-45 opacity-20 z-10"
+		/>
+		<img
+			src="/images/projects/app-aemers/app-aemers-dash-home.png"
+			alt="Project"
+			class="object-cover rounded-t-lg w-11/12 z-20 mt-32"
+		/>
+	</div>
+	<a href="/">
+		<h6 class="font-light text-white text-lg md:text-xl tracking-wide hover:underline mt-4">
+			app.aemers - College Application Tracker
+		</h6>
+	</a>
+</div>
--- src/routes/(root)/Projects.svelte
+++ src/routes/(root)/Projects.svelte
@@ -1,25 +1,14 @@
 <script lang="ts">
 	import TitleChip from '$lib/components/layout/TitleChip.svelte';
 	import PajamasProject from '~icons/pajamas/project';
-	import { filter, GreenFall } from '@skeletonlabs/skeleton';
-</script>
 
-<GreenFall />
+	import AppAemers from './(projects)/AppAemers.svelte';
+</script>
 
 <section id="projects" class="flex flex-col py-20">
 	<TitleChip icon={PajamasProject} title="Projects" />
 	<h2 class="text-white text-2xl md:text-5xl font-light mb-8">
 		My <span class="text-primary-500">Projects</span>
 	</h2>
-	<img
-		use:filter={'#GreenFall'}
-		src="/images/project.jpg"
-		alt="Project"
-		class="object-cover max-w-2xl w-full rounded-3xl mb-6"
-	/>
-	<a href="/">
-		<h6 class="font-light text-white text-lg md:text-xl tracking-wide hover:underline">
-			app.aemers - College Application Tracker
-		</h6>
-	</a>
+	<AppAemers />
 </section>
