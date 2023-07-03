6650dadfcae94fd66a3515b7328fc04cb72bdd5f
Hasibuzzaman Tonmoy <hsb.tonmoy@gmail.com>
2023-07-03 15:45:52 -0400

    added project carousel

--- src/routes/(root)/(projects)/AppAemers.svelte
+++ src/routes/(root)/(projects)/AppAemers.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-	import { filter, GreenFall } from '@skeletonlabs/skeleton';
+	import Carousel from 'svelte-carousel';
+	import { browser } from '$app/environment';
+
+	import MaterialSymbolsArrowBackIos from '~icons/material-symbols/arrow-back-ios';
+	import MaterialSymbolsArrowForwardIos from '~icons/material-symbols/arrow-forward-ios';
+
+	let carousel: any;
+
+	const images = [
+		'app-aemers-dash-home.png',
+		'app-aemers-app-tour.png',
+		'app-aemers-doc-upload.png',
+		'app-aemers-form.png'
+	];
 </script>
 
 <div class="">
@@ -9,13 +22,25 @@
 		<img
 			src="/images/projects/app-aemers/app-aemers-login.png"
 			alt="Project"
-			class="absolute object-fill w-full -rotate-45 opacity-20 z-10"
+			class="absolute object-fill w-full -rotate-45 opacity-20"
 		/>
-		<img
-			src="/images/projects/app-aemers/app-aemers-dash-home.png"
-			alt="Project"
-			class="object-cover rounded-t-lg w-11/12 z-20 mt-32"
-		/>
+		{#if browser}
+			<Carousel let:showPrevPage let:showNextPage bind:this={carousel} dots={false} class="z-20">
+				<button slot="prev" on:click={showPrevPage} class="flex items-center justify-center px-2">
+					<MaterialSymbolsArrowBackIos />
+				</button>
+				{#each images as img, index}
+					<img
+						src="/images/projects/app-aemers/{img}"
+						alt="Project"
+						class="object-cover rounded-t-lg w-full md:w-11/12 mt-20 md:mt-32"
+					/>
+				{/each}
+				<button slot="next" on:click={showNextPage} class="flex items-center justify-center px-2">
+					<MaterialSymbolsArrowForwardIos />
+				</button>
+			</Carousel>
+		{/if}
 	</div>
 	<a href="/">
 		<h6 class="font-light text-white text-lg md:text-xl tracking-wide hover:underline mt-4">
